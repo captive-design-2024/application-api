a881c19b3983074e04c43f80815f43eb4e4cbf87
jaeyong <ljy3573507@gmail.com>
2024-09-27 15:02:36 +0900

    change controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,10 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { JwtAuthGuard } from './auth/guard/jwt.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly userService: UserService,
     ) {}
 
   @Get()
@@ -15,10 +12,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('Mypage')
-  @UseGuards(JwtAuthGuard)
-  async sendNameAndEmail(@Req() req) {
-    const findUser = await this.userService.findByLoginId(req.user.id);
-    return [{ name: findUser.user_name }, { email: findUser.email }]
-  }
 }
