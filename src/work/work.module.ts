3b51c7f74d39236f52d44f09ca016b72522d7c27
joonseokchung <joonseok99@naver.com>
2024-10-01 19:39:28 +0900

    fix: prisma provider

--- src/work/work.module.ts
+++ src/work/work.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { WorkService } from './work.service';
 import { WorkController } from './work.controller';
 import { HttpModule } from '@nestjs/axios';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [WorkController],
-  providers: [WorkService],
+  providers: [WorkService, PrismaService],
   exports: [WorkService],
 })
 export class WorkModule {}
