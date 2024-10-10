affe9c86014e85f802339f09370a7e0f84ddafc3
jaeyong <ljy3573507@gmail.com>
2024-10-10 17:18:53 +0900

    projectModule

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from '../prisma.service';
 import { ProjectService } from './project.service';
 import { ProjectController, ProjectController2 } from "./project.controller";
 import { UserModule } from '../user/user.module';
+import { FilesModule } from "../files/files.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, FilesModule],
   controllers: [ProjectController, ProjectController2],
   providers: [PrismaService, ProjectService],
 })
