c2b162ccde256040aef48456fc5fb0b886521443
jaeyong <ljy3573507@gmail.com>
2024-10-11 16:48:51 +0900

    commit

--- src/youtube/youtube.module.ts
+++ src/youtube/youtube.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { YoutubeService } from './youtube.service';
 import { YoutubeController } from './youtube.controller';
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
+  imports: [AuthModule],
   providers: [YoutubeService],
   controllers: [YoutubeController]
 })
